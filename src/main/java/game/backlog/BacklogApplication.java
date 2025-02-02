03b749ed8d81ac156f86eb1736738cfad32ef0e8
emarkula24 <emarkula24@student.oulu.fi>
2025-02-02 11:47:52 +0200

    first commit

---
+++ src/main/java/game/backlog/BacklogApplication.java
@@ -0,0 +1,18 @@
+package game.backlog;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BacklogApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
