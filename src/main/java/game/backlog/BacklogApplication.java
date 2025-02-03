d66b2fdf9ed6c1b39eb8c5c437d6ecadf9c71715
emarkula24 <emarkula24@student.oulu.fi>
2025-02-03 14:09:11 +0200

    Added elementary search endpoint

--- src/main/java/game/backlog/BacklogApplication.java
+++ src/main/java/game/backlog/BacklogApplication.java
@@ -1,9 +1,16 @@
 package game.backlog;
+import org.apache.coyote.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
@@ -14,5 +21,18 @@ public class BacklogApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+	private static final String API_KEY = "9329acb5599a035b68fd6bdea694cce3d82a7063"; // Replace with your actual API key
+	private static final String BASE_URL = "http://www.giantbomb.com/api/search/";
+
+	@GetMapping("/search")
+	public ResponseEntity<String> searchGame(@RequestParam String query) {
+		String url = "http://www.giantbomb.com/api/search/?api_key=9329acb5599a035b68fd6bdea694cce3d82a7063&format=json&query=" + query + "&resources=game";
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+		System.out.println(response);
+		return response;
 	}
 }
