ef807aa18a0a40a47514d837c84c652c43ae41b8
emarkula24 <emarkula24@student.oulu.fi>
2025-02-03 14:20:22 +0200

    Updated to using restclient

--- src/main/java/game/backlog/BacklogApplication.java
+++ src/main/java/game/backlog/BacklogApplication.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
@@ -18,21 +19,22 @@ public class BacklogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BacklogApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+
 	private static final String API_KEY = "9329acb5599a035b68fd6bdea694cce3d82a7063"; // Replace with your actual API key
 	private static final String BASE_URL = "http://www.giantbomb.com/api/search/";
 
 	@GetMapping("/search")
-	public ResponseEntity<String> searchGame(@RequestParam String query) {
+	public ResponseEntity<String> searchGameClient(@RequestParam String query) {
 		String url = "http://www.giantbomb.com/api/search/?api_key=9329acb5599a035b68fd6bdea694cce3d82a7063&format=json&query=" + query + "&resources=game";
 
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-		System.out.println(response);
-		return response;
+		RestClient defaultClient = RestClient.create();
+		ResponseEntity<String> result = defaultClient.get()
+				.uri("http://www.giantbomb.com/api/search/?api_key=9329acb5599a035b68fd6bdea694cce3d82a7063&format=json&query={quey}&resources=game", query)
+				.retrieve()
+				.toEntity(String.class);
+		System.out.println("Response status: " + result.getStatusCode());
+		System.out.println("Response headers: " + result.getHeaders());
+		System.out.println("Contents: " + result.getBody());
+		return result;
 	}
 }
