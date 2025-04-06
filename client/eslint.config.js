c87a72a98ccecae62c895fece4460677579cc013
emarkula24 <emarkula24@student.oulu.fi>
2025-04-06 21:11:51 +0300

    tanstack router setup

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,34 +1,28 @@
-
-// import globals from 'globals'
-// import reactHooks from 'eslint-plugin-react-hooks'
-// import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import eslint from '@eslint/js'
 
 export default tseslint.config(
-  // { ignores: ['dist'] },
-  // {
-  //   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-  //   files: ['**/*.{ts,tsx}'],
-  //   languageOptions: {
-  //     ecmaVersion: 2020,
-  //     globals: globals.browser,
-  //   },
-
-  //   plugins: {
-  //     'react-hooks': reactHooks,
-  //     'react-refresh': reactRefresh,
-  //     '@typescript-esling': tseslint.plugin,
-  //   },
-  //   rules: {
-  //     ...reactHooks.configs.recommended.rules,
-  //     'react-refresh/only-export-components': [
-  //       'warn',
-  //       { allowConstantExport: true },
-  //     ],
-  //   },
-  // },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
 )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    // ...,
   ],
 })
